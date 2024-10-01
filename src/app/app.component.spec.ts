6fc7298b5fa9a5f80824bf7d5a4762eb01c87518
Ngor SECK <Ngor.SECK@akkodis.com>
2024-10-01 18:24:48 +0200

    Update Unit Test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('httpclient-project');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, httpclient-project');
-  });
 });
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./forms/template/login/login.component";
+import {RegistryComponent} from "./forms/reactive/registry/registry.component";
+import {HomeComponent} from "./layout/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: "register",
+    component: RegistryComponent
+  },
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "/login"
+  }
+];
